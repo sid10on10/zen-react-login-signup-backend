12631bb73f4c4c01e9966a54ee75358df00d1b1c
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-10-19 09:28:28 +0530

    added reset support

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.post("/reset_password",async function(req,res,next){
         let reset_string = Math.random().toString(36).substr(2, 5);
         let update = await db.collection("users").findOneAndUpdate({email},{$set:{reset_token:reset_string}})
         let payload = `Reset Your Password here http://localhost:3000/reset/${userId}/${reset_string}`
-        let sending = sendEmail(email,"Reset Link",payload)
+        let sending = await sendEmail(email,"Reset Link",payload)
         console.log(sending)
         res.json({
           message:"Email sent check your email for reset password"
@@ -36,5 +36,65 @@ router.post("/reset_password",async function(req,res,next){
     console.log(error)
   }
 })
+
+router.get('/reset/:userid/:reset_string', async function(req, res, next) {
+  let client;
+  try{
+    client = await mongodClient.connect(url)
+    let db = client.db("react")
+    let user = await db.collection("users").findOne({_id:mongodb.ObjectId(req.params.userid)})
+    if(user){
+      let userEmail = user.email
+      if(user.reset_token==req.params.reset_string){
+        res.json({
+          message:"Valid RESET URL"
+        })
+        client.close()
+      }else{
+        client.close()
+        res.status(404).json({
+          message:"Invalid URL"
+        })
+      }
+    }else{
+      client.close()
+      res.status(404).json({
+        message:"Invalid URL"
+      })
+    }
+    
+    }catch(error){
+      client.close()
+      console.log(error)
+  }
+});
+
+router.post('/reset', async function(req, res, next) {
+  let client;
+  try{
+    client = await mongodClient.connect(url)
+    let db = client.db("react")
+    let {email,password} = req.body
+    let user = await db.collection("users").findOne({email:email})
+    if(user){
+      let salt = await bcryptjs.genSalt(10)
+      let hash = await bcryptjs.hash(password,salt)
+      password = hash
+      let setpass = await db.collection("users").updateOne({email},{$set:{password}})
+      let remove_token = await db.collection("users").updateOne({email},{$unset:{reset_token:1}})
+      res.json({
+        message:"Password reset complete"
+      })
+    }else{
+      res.json({
+        message:"No user found with this email"
+      })
+    }
+    
+  }catch(error){
+    client.close()
+    console.log(error)
+  }
+});
 
 module.exports = router;
