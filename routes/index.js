e4f6c040f28110eea2722ad501eec227068d84c2
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-10-19 09:30:36 +0530

    fixed bug

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var {url,mongodClient} = require("../config")
 const { sendEmail } = require('../common/mailer');
+var mongodb = require("mongodb")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
